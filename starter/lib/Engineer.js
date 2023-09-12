35fbad8591ea83d5a4d5d84de7701077cfe12f89
Saad-054 <saad.siddiqui@msn.com>
2023-09-12 22:18:27 +0100

    intern.js

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,4 +1,4 @@
-// TODO: code to define and export the Engineer class.  
+// code to define and export the Engineer class.  
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
