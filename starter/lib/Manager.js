1b02585c1cfa5ea3ebf0079d8661cd5938bdd89c
Saad-054 <saad.siddiqui@msn.com>
2023-09-12 22:19:14 +0100

    manager.js

---
+++ starter/lib/Manager.js
@@ -0,0 +1,18 @@
+// code to define and export the Manager class. 
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+
+    getRole() {
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
